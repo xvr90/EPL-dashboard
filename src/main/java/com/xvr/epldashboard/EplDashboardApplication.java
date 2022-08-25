b5b7fe61b06842721e63b0a8d16e940ae69f8f59
Javier Ramirez <javier.ramirez.0690@gmail.com>
2022-08-25 00:53:09 -0500

    Initial commit EPL-Dashboard

---
+++ src/main/java/com/xvr/epldashboard/EplDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.xvr.epldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplDashboardApplication.class, args);
+	}
+
+}
