49912761d3be73aaf69456a5086d2d3515c6d56e
Javier Ramirez <javier.ramirez.0690@gmail.com>
2022-08-25 01:14:56 -0500

    Adding MatchInput entity and MatchDataProcessor

---
+++ src/main/java/com/xvr/epldashboard/data/MatchInput.java
@@ -0,0 +1,91 @@
+package com.xvr.epldashboard.data;
+
+public class MatchInput {
+    
+private String season;
+private String pos;
+private String team;
+private String pld;
+private String win;
+private String draw;
+private String lost;
+private String gf;
+private String ga;
+private String gd;
+private String pts;
+private String qualification_or_relegation;
+
+public String getSeason() {
+    return season;
+}
+public void setSeason(String season) {
+    this.season = season;
+}
+public String getPos() {
+    return pos;
+}
+public void setPos(String pos) {
+    this.pos = pos;
+}
+public String getTeam() {
+    return team;
+}
+public void setTeam(String team) {
+    this.team = team;
+}
+public String getPld() {
+    return pld;
+}
+public void setPld(String pld) {
+    this.pld = pld;
+}
+public String getWin() {
+    return win;
+}
+public void setWin(String win) {
+    this.win = win;
+}
+public String getDraw() {
+    return draw;
+}
+public void setDraw(String draw) {
+    this.draw = draw;
+}
+public String getLost() {
+    return lost;
+}
+public void setLost(String lost) {
+    this.lost = lost;
+}
+public String getGf() {
+    return gf;
+}
+public void setGf(String gf) {
+    this.gf = gf;
+}
+public String getGa() {
+    return ga;
+}
+public void setGa(String ga) {
+    this.ga = ga;
+}
+public String getGd() {
+    return gd;
+}
+public void setGd(String gd) {
+    this.gd = gd;
+}
+public String getPts() {
+    return pts;
+}
+public void setPts(String pts) {
+    this.pts = pts;
+}
+public String getQualification_or_relegation() {
+    return qualification_or_relegation;
+}
+public void setQualification_or_relegation(String qualification_or_relegation) {
+    this.qualification_or_relegation = qualification_or_relegation;
+}
+
+}
